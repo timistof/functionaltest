b855e50d068f98480eca0f7660ab33f459c73f4d
Tim Walther <timistof@gmail.com>
2021-01-06 17:22:32 +0100

    Added flash.sh, flashing now works. Now to repair the test again :/

---
+++ flash.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -z "$1" ]
+then 
+    echo "firmware path not specified.";
+else
+    /usr/bin/openocd -s /usr/share/openocd/scripts -f ./timetosser.cfg -c "program $1 verify reset exit 0x08000000"
+fi
